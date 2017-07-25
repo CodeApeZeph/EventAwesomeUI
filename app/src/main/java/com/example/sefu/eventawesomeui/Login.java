150cdcf47dc8bf472e8d5b2cf66518660923b95e
CodeApeZeph <zephaniahsefuoduori@gmail.com>
2017-07-25 21:35:07 +0300

    first commit

---
+++ app/src/main/java/com/example/sefu/eventawesomeui/Login.java
@@ -0,0 +1,25 @@
+package com.example.sefu.eventawesomeui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signUp = (Button) findViewById(R.id.signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(signUpIntent);
+            }
+        });
+    }
+}
