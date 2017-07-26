3fde667e90afdba283bf64ceb82c82df5a21853a
CodeApeZeph <zephaniahsefuoduori@gmail.com>
2017-07-26 17:32:54 +0300

    walkthrough update

--- app/src/main/java/com/example/sefu/eventawesomeui/Signup.java
+++ app/src/main/java/com/example/sefu/eventawesomeui/Signup.java
@@ -1,13 +1,26 @@
 package com.example.sefu.eventawesomeui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
+    Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        btnCreate = (Button) findViewById(R.id.create);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent homeIntent = new Intent(getApplicationContext(), Walkthrough.class);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
