57ea74c262f96dc6ae1d1d349a9fa3d2d1f5e580
CodeApeZeph <zephaniahsefuoduori@gmail.com>
2017-08-01 20:53:25 +0300

    home mod

--- app/src/main/java/com/example/sefu/eventawesomeui/Home.java
+++ app/src/main/java/com/example/sefu/eventawesomeui/Home.java
@@ -1,14 +1,11 @@
 package com.example.sefu.eventawesomeui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.sefu.eventawesomeui.adapter.HomeEventAdapter;
@@ -31,18 +28,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 
 
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view_two);
@@ -72,7 +57,5 @@ public class Home extends AppCompatActivity {
 
         adapter = new HomeEventAdapter(data);
         recyclerView.setAdapter(adapter);
-
-
     }
 }
